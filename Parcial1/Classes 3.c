44a71b0891d3d95f585f38ff2e07a483812ececf
Francisco Manuel Suárez <142342067+Paco-S23@users.noreply.github.com>
2025-08-26 20:19:49 -0600

    Add files via upload

---
+++ Parcial1/Classes 3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n Size of 'int' primitive data type: %llu", sizeof(int));
+    printf("\n Size of 'float' primitive data type: %llu", sizeof(float));
+    printf("\n Size of 'double' primitive data type: %llu", sizeof(double));
+    printf("\n Size of 'char' primitive data type: %llu", sizeof(char));
+}
